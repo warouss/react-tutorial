5b98f9090e47d2509bcab09f2d791ceb864c8d15
Walid Arouss <warouss@aliznet.fr>
2020-04-10 13:46:42 +0200

    init Game

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders Game", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Tic Tac Toe/i);
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/components/square/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./index.css";
+
+export default class Square extends React.Component {
+  render() {
+    return <button className="square">{/* TODO */}</button>;
+  }
+}
