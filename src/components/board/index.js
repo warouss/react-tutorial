23de77bc60d587492f7fba7b06554e4665c4f570
Walid Arouss <warouss@aliznet.fr>
2020-04-13 11:51:40 +0200

    Passer des données et des fonctions via les props

--- src/components/board/index.js
+++ src/components/board/index.js
@@ -3,8 +3,27 @@ import Square from "../square";
 import "./index.css";
 
 export default class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    //slice créé une copie du tableau
+    const squares = this.state.squares.slice();
+    squares[i] = "X";
+    this.setState({ squares: squares });
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
--- src/components/square/index.js
+++ src/components/square/index.js
@@ -2,16 +2,9 @@ import React from "react";
 import "./index.css";
 
 export default class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
   render() {
     return (
-      <button className="square" onClick={() => this.setState({ value: "X" })}>
+      <button className="square" onClick={() => this.props.onClick()}>
         {this.props.value}
       </button>
     );
