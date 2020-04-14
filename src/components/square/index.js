e1fcc2b9409506724c6d18cd7503527d5aa2918a
Walid Arouss <warouss@aliznet.fr>
2020-04-14 10:52:14 +0200

    Prop-types

--- src/components/square/index.js
+++ src/components/square/index.js
@@ -1,7 +1,12 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "./index.css";
 
 export default class Square extends React.Component {
+  static propTypes = {
+    value: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired,
+  };
   render() {
     return (
       <button className="square" onClick={() => this.props.onClick()}>
